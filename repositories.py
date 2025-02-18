a8509ed9096a2302d2f1f3f5f3275f188068b874
WilliamParra <rikder13@gmail.com>
2025-02-18 20:07:33 +0100

    añadido clase para manejo de usuarios con la base de datos

---
+++ repositories.py
@@ -0,0 +1,16 @@
+class UserRepository:
+    def __init__(self, cursor):
+        self.cursor = cursor
+        
+    def create(self, name, age):
+        pass
+    
+    def get_all(self):
+        pass
+    
+    def search_by_name(self, name):
+        pass
+    
+    def get_by_id(self, id):
+        pass
+    
