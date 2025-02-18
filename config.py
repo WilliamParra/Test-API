146493e43d41c65fa64e1aadcaf1e5766c42a079
WilliamParra <rikder13@gmail.com>
2025-02-18 19:43:54 +0100

    añadida clase configuracion

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    DATABASE = {
+        "dbname": "postgres",
+        "user": "postgres",
+        "password": "admin1",
+        "host": "localhost",
+        "port": 5433
+    }
+    CORS_ORIGIN = "http://127.0.0.1:5500"
