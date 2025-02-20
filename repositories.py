1e6c6eac673203b639b40f9428ed84a5f106a347
WilliamParra <rikder13@gmail.com>
2025-02-21 00:07:00 +0100

    Creado archivo para consulta a BBDD

--- repositories.py
+++ repositories.py
@@ -1,16 +1,40 @@
+from models import User
+
+
 class UserRepository:
     def __init__(self, cursor):
         self.cursor = cursor
-        
+
     def create(self, name, age):
-        pass
-    
+        self.cursor.excecute(
+            "INSERT INTO users (name, age) VALUES (%s, %s)", (name, age)
+        )
+
     def get_all(self):
-        pass
-    
+        self.cursor.execute("SELECT id, name, age FROM users")
+        rows = self.cursor.fetchall()
+        return [User.from_db_row(row) for row in rows]
+
     def search_by_name(self, name):
-        pass
-    
+        self.cursor.execute(
+            """
+                SELECT id, name, age 
+                FROM users
+                WHERE LOWER(SPLIT_PART(name,' ',1)) = LOWER(%s)
+            """,
+            (name,),
+        )
+        rows = self.cursor.fetchall()
+        return [User.from_db_row(row) for row in rows]
+
     def get_by_id(self, id):
-        pass
-    
+        self.cursor.execute(
+            """
+                SELECT id, name, age
+                FROM users
+                WHERE id = %s
+            """,
+            (id,),
+        )
+        row = self.cursor.fetchone()
+        return User.from_db_row(row) if row else None
